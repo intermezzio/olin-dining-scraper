155def5c9f294f64a40dc36daa26513d65440d37
Andrew Mascillaro <amascillaro@gmail.com>
2022-03-01 18:31:30 -0500

    Add all other scraping info

--- scrape.py
+++ scrape.py
@@ -5,7 +5,8 @@ from icecream import ic
 
 URL = "https://rebeccasculinarygroup.com/olin/menu-items/"
 
-weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+all_days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+weekdays = all_days[:5]
 
 class DiningInfoManager:
 	def __init__(self):
@@ -21,26 +22,92 @@ class DiningInfoManager:
 
 	def get_breakfasts(self):
 		breakfast_info = self.all_details[0]
-		node = breakfast_info.find("span")
-		assert node.text == "Breakfast"
-
-		self.breakfast_dict = dict()
-
-		weekday_index = 0
-		weekday = ic(weekdays[weekday_index])
-		for node in breakfast_info.find_all(recursive=False):
-			if weekday in node.text:
-				self.breakfast_dict[weekday] = ""
-				weekday_index += 1
-				weekday = ic(weekdays[weekday_index]) if weekday_index < len(weekdays) else "Saturday"
-			elif ic(weekday_index) >= 1:
-				self.breakfast_dict[weekdays[ic(weekday_index-1)]] += str(node.text) + "\n"
-			node = ic(node.find_next())
+		self.breakfast_dict = self._get_section(breakfast_info,
+			weekdays, "Breakfast")
 		return self.breakfast_dict
+
+	def get_lunch_specials(self):
+		lunch_specials_info = self.all_details[2]
+		at_specials_section = False
+		current_days_in_header = set()
+		self.lunch_specials_dict = {day: "" for day in all_days}
+
+		for node in lunch_specials_info.find_all(recursive=False):
+			if not at_specials_section:
+				if "Lunch Specials" in node.text:
+					at_specials_section = True
+				else:
+					continue
+			potential_days_in_header = set()
+			for day in all_days:
+				if day in node.text:
+					potential_days_in_header.add(day)
+
+			print(potential_days_in_header)
+			if potential_days_in_header:
+				current_days_in_header = potential_days_in_header
+				continue
+			
+			for day in current_days_in_header:
+				self.lunch_specials_dict[day] += str(node.text) + "\n"
+
+		return self.lunch_specials_dict
+
+	def get_entrees(self):
+		entrees_info = self.all_details[3]
+		self.lunch_entree_dict = self._get_section(entrees_info,
+			weekdays, "Lunch", "Saturday")
+		self.dinner_entree_dict = self._get_section(entrees_info,
+			all_days, "Dinner")
+		return (self.lunch_entree_dict, self.dinner_entree_dict)
+
+	def get_grill(self):
+		grill_info = self.all_details[4]
+		self.lunch_grill_dict = self._get_section(grill_info,
+			weekdays, "Lunch", "Dinner")
+		self.dinner_grill_dict = self._get_section(grill_info,
+			all_days, "Dinner")
+		return (self.lunch_grill_dict, self.dinner_grill_dict)
+
+	def get_pizza(self):
+		pizza_info = self.all_details[5]
+		self.pizza_dict = self._get_section(pizza_info,
+			weekdays, "Available")
+		return self.pizza_dict
+
+	def _get_section(self, subsoup, dotw, start_str=None, end_str=None):
+		has_started = start_str is None
+		self.section_dict = {day: "" for day in dotw}
+
+		dotw_index = 0
+		day = dotw[dotw_index]
+		for node in subsoup.find_all(recursive=False):
+			if not has_started:
+				if start_str in node.text:
+					has_started = True
+				else:
+					continue
+			if end_str and end_str in node.text:
+				break
+
+			if day in node.text:
+				dotw_index += 1
+				day = dotw[dotw_index] if dotw_index < len(dotw) else "Monday"
+			elif dotw_index >= 1:
+				self.section_dict[dotw[dotw_index-1]] += str(node.text) + "\n"
+			node = node.find_next()
+		return self.section_dict
+
+
 
 if __name__ == "__main__":
 	d = DiningInfoManager()
-	breakfasts = d.get_breakfasts()
-	print(breakfasts)
-	with open("breakfast.txt", "w") as outfile:
-		json.dump(breakfasts, outfile, indent=4)
+	# lunch_specials = d.get_lunch_specials()
+	# print(lunch_specials)
+	# with open("lunch_specials.txt", "w") as outfile:
+	# 	json.dump(lunch_specials, outfile, indent=4)
+
+	entrees = d.get_entrees()
+	print(entrees)
+	with open("entrees.txt", "w") as outfile:
+		json.dump(entrees, outfile, indent=4)
