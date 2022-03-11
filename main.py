7328901bb0545553e77c348981da0a9b71ebdb45
Andrew Mascillaro <amascillaro@gmail.com>
2022-03-11 12:38:57 -0500

    Read an array of email addresses from an environment variable

--- main.py
+++ main.py
@@ -3,17 +3,30 @@ import time
 import datetime
 from message import MessageGenerator
 from send import send_mail
+import os
 
 m = MessageGenerator()
 
 def get_meal(day, meal):
-	print(f"Getting {day} {meal}")
-	global m
-	msg = m.generate_message(day, meal, quotes=3)
-	send_mail(recipient="amascillaro@olin.edu",
-		subject=f"{day} {meal} at the Dining Hall",
-		body=msg
-	)
+	try:
+		print(f"Getting {day} {meal}")
+		global m
+		msg = m.generate_message(day, meal, quotes=3)
+		email_addr_str = os.environ.get("EMAIL_ADDRS", '')
+		email_addrs = list(filter(lambda x: x, email_addr_str.split(",")))
+		print(f"{len(email_addrs)} email addresses")
+		for addr in email_addrs:
+			send_mail(recipient=addr,
+				subject=f"{day} {meal} at the Dining Hall",
+				body=msg
+			)
+	except Exception as e:
+		print("Exception")
+		print(e)
+		send_mail(recipient="amascillaro@olin.edu",
+			subject=f"Olin Dining Scraper Error",
+			body=e
+		)
 
 schedule.every().monday.at("11:07").do(lambda: get_meal("Monday", "Breakfast"))
 schedule.every().tuesday.at("11:07").do(lambda: get_meal("Tuesday", "Breakfast"))
@@ -35,7 +48,7 @@ schedule.every().friday.at("20:44").do(lambda: get_meal("Friday", "Dinner"))
 schedule.every().saturday.at("20:44").do(lambda: get_meal("Saturday", "Dinner"))
 schedule.every().sunday.at("20:44").do(lambda: get_meal("Sunday", "Dinner"))
 
-while True:
-    schedule.run_pending()
-    print(f"Dormant {datetime.datetime.now()}")
-    time.sleep(30)
+# while True:
+#     schedule.run_pending()
+#     print(f"Dormant {datetime.datetime.now()}")
+#     time.sleep(30)
